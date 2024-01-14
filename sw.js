3fa53a343e06805157c393d7f08ddf6d7e150adf
mikejed <michael@mkgarrison.com>
2024-01-13 20:44:20 -0700

    Increased size of score inputs

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-13_3";
+const cacheName = "CocoNutsScouting_2024-01-13_4";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
