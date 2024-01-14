14342f4aaa2057f85e43d29edf227e2480c07b46
mikejed <michael@mkgarrison.com>
2024-01-13 20:50:50 -0700

    Made label layouts uniform

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-13_4";
+const cacheName = "CocoNutsScouting_2024-01-13_5";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
