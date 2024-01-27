30ae0a829bb74d11c8eb715cc6b0d704c3cbfd76
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 16:49:46 -0700

    Put no back in collum

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_7";
+const cacheName = "CocoNutsScouting_2024-01-27_8";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
