da2693d49e7fb50897d198ffc2777e0ba9edb9a8
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 14:55:14 -0700

    Made broken and Disabled seperate collums instead of rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_4";
+const cacheName = "CocoNutsScouting_2024-01-27_5";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
