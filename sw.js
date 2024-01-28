92549dadf8d0c509db17472f0c1a9a02476eeb74
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 20:27:24 -0700

    moved role into the same collum as climb and pickup location

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_13";
+const cacheName = "CocoNutsScouting_2024-01-27_14";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
