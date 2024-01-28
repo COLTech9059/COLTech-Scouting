54f15b41dd4c0b8d3ca29403eb96efa950b06cdb
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 20:23:42 -0700

    Added robot role

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_12";
+const cacheName = "CocoNutsScouting_2024-01-27_13";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
