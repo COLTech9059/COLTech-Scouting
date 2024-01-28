0da5eaa172cfd9a66384eccf2206d6a1993a26a1
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 20:13:33 -0700

    Switched Climb and Broken/disabled rows

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_11";
+const cacheName = "CocoNutsScouting_2024-01-27_12";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
