410f91c14cadd6f65c1e12fd8911b3e84cf2e355
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 20:05:29 -0700

    added invalid feedback notes to broken and disabled

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_10";
+const cacheName = "CocoNutsScouting_2024-01-27_11";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
