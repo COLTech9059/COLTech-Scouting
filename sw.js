d8033bedfac7be2ac1b947efe262c8e2008b2f66
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-27 19:59:14 -0700

    Put Broken and Disabled in the same row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-27_9";
+const cacheName = "CocoNutsScouting_2024-01-27_10";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
