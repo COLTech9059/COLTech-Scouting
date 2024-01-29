1dee386f976976d27c807a745d7c70ca5daf72ba
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-01-29 11:06:51 -0700

    Changed invalid feedback for role

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_1";
+const cacheName = "CocoNutsScouting_2024-01-29_3";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
