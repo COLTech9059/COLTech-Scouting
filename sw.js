c034997f142f5c549938c865643ff1f443648c8d
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2024-01-29 17:59:58 -0700

    Changed color again

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_5";
+const cacheName = "CocoNutsScouting_2024-01-29_6";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
