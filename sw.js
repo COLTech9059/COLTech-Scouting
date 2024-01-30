00327e451c32f2e6acce6eddc9c6bd143837714e
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-01-29 18:55:09 -0700

    fixed role id and value

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_12";
+const cacheName = "CocoNutsScouting_2024-01-29_13";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
