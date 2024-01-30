547944ee8eab024af1771c7dae470892a861fb3e
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-01-29 19:12:08 -0700

    Changed role to include neither

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_15";
+const cacheName = "CocoNutsScouting_2024-01-29_16";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
