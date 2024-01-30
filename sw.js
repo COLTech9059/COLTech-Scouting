67e21951caa368a703e65027f60185d8992d4a17
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-01-29 19:02:53 -0700

    Added another collum

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_13";
+const cacheName = "CocoNutsScouting_2024-01-29_14";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
