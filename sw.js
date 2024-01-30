72136b0732c2a3aa402344cc1ceef94d9875f744
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2024-01-29 17:57:33 -0700

    Changed invalid comments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_4";
+const cacheName = "CocoNutsScouting_2024-01-29_5";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
