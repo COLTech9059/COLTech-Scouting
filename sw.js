b77203ac05dfc4cb6ac4b21a2fec1601c4a728a2
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-01-29 18:44:52 -0700

    put broken and disabled in the same card as information

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_10";
+const cacheName = "CocoNutsScouting_2024-01-29_11";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
