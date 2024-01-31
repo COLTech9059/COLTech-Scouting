a37545868cbdcd94901150543c1341604cb9c79e
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-01-30 17:52:45 -0700

    Put broken and disabled on seperate row

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-01-29_16";
+const cacheName = "CocoNutsScouting_2024-01-30_1";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
