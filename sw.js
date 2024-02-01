3cadd084a208638a0a7ac526cfaaaf0e9daf340a
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2024-02-01 15:46:24 -0700

    again

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-1_2";
+const cacheName = "CocoNutsScouting_2024-02-1_3";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
