c4816222dee6e8c439f3b35bb6f8f87826908bb2
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2024-02-01 15:41:49 -0700

    valid text color

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-1_1";
+const cacheName = "CocoNutsScouting_2024-02-1_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
