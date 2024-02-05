2d389b73ee34501ca9575b6530e5b1f10a44e008
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-05 08:54:22 -0700

    Fixed names for themes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-5_1";
+const cacheName = "CocoNutsScouting_2024-02-5_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
