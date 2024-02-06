d79a6b5295dbb613f31a11394cd7a879c86c9400
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-02-05 17:00:07 -0700

    theme switch attempt 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-5_2";
+const cacheName = "CocoNutsScouting_2024-02-5_3";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
