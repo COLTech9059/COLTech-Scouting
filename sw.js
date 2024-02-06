b50cce4803509c5f4d20340f4be51f904b7f46a1
Connor Norris <99221420+ConnorJNorris@users.noreply.github.com>
2024-02-05 18:06:01 -0700

    reverted changes

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-5_15";
+const cacheName = "CocoNutsScouting_2024-02-5_16";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
