23771776254d0b13a24306a55264a36d51256e38
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-02-05 17:45:43 -0700

    added comments

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-5_12";
+const cacheName = "CocoNutsScouting_2024-02-5_13";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
