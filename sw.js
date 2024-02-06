31e193ba5d1b7592bba22aaf006d9bd99e9d0782
OscarCDavis <156030170+OscarCDavis@users.noreply.github.com>
2024-02-05 17:42:08 -0700

    Removed that tag

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-5_11";
+const cacheName = "CocoNutsScouting_2024-02-5_12";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
