38bf335f4708941590ddab76050b9a86ddfe8c34
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 11:00:56 -0700

    Added spirit mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_1";
+const cacheName = "CocoNutsScouting_2024-02-10_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
