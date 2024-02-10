898f0949559daa98d3a63b931139ef1f1731c47a
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 14:32:44 -0700

    changed the theme buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_11";
+const cacheName = "CocoNutsScouting_2024-02-10_12";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
