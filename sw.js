fb231065ed8c63c2098d612650d5d37f5d6f8358
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 11:09:12 -0700

    added input, select, and text area to dark

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_2";
+const cacheName = "CocoNutsScouting_2024-02-10_3";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
