b3279114e0de2dcff1db50b891c5ea735267dd8a
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 14:13:55 -0700

    Changed all the values at the bottom for data

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_9";
+const cacheName = "CocoNutsScouting_2024-02-10_10";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
