f3d8ae8dae1c44edd1e2a4abeee82088791ded6d
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 11:35:06 -0700

    changed colors more

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_7";
+const cacheName = "CocoNutsScouting_2024-02-10_8";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
