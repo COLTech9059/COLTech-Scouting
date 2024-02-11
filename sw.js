ab25d0d2800e479ef9a5e68368319f3e4c466f4e
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 19:43:30 -0700

    turned theme toggles to buttons

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_17";
+const cacheName = "CocoNutsScouting_2024-02-10_18";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
