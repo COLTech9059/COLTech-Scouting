2ecd18f159e45631e1a9470a6cfb77527d32ac3e
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-10 20:36:25 -0700

    fixed plasma mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-10_21";
+const cacheName = "CocoNutsScouting_2024-02-10_23";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
