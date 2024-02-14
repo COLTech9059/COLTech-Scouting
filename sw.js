48a29aded72ce6ec3dded1030168c145a5ee351a
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-14 12:12:58 -0700

    Tried new stuff for formating

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-13_2";
+const cacheName = "CocoNutsScouting_2024-02-14_1";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
