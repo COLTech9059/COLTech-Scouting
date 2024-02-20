e5493f2572527e785955285ab96339540cfd7999
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-20 10:44:36 -0700

    more formating

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-20_4";
+const cacheName = "CocoNutsScouting_2024-02-20_5";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
