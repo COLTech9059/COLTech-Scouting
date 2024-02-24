31bef91f1325193ad5ad4a238d3c372842f5b0b6
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-23 19:29:59 -0700

    uncamelcased some stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-23_3";
+const cacheName = "CocoNutsScouting_2024-02-23_4";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
