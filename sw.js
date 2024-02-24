0c5aa04810fe40a9cfd292ca757f1763a7f28289
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-24 11:37:03 -0700

    changed team options for week 0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-23_4";
+const cacheName = "CocoNutsScouting_2024-02-24_1";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
