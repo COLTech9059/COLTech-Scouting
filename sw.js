72a036fbecb97a96573aa8f3e9c4708d419d47e7
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-28 20:54:19 -0700

    Copy pasted a lot of stuff

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-2_1";
+const cacheName = "CocoNutsScouting_2024-02-2_2";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
