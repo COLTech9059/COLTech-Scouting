7a400e3aa3ab34eb2ac8acd901d96e1306f2ea56
AquaShock211 <143662909+AquaShock211@users.noreply.github.com>
2024-02-29 09:22:06 -0700

    Fixed stuff again

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheName = "CocoNutsScouting_2024-02-29_3";
+const cacheName = "CocoNutsScouting_2024-02-29_4";
 const precacheResources = [
     "/CocoNuts-Scouting/",
     "/CocoNuts-Scouting/index.html",
